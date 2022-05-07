515ca672d3d5527d4a3d5811436fc3a6a681ea12
Muppet <thechinesemuppet@gmail.com>
2022-05-07 19:54:44 +0530

    fixed deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: "static",
+  ssr: false,
   typescript: {
     shim: false,
+  },
+  generate: {
+    fallback: "true",
   },
 });
