cd098c1e099731ba2bc75f6a9d28a0173070d629
Muppet <thechinesemuppet@gmail.com>
2022-05-08 17:36:21 +0530

    worked on stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,109 +1,135 @@
 <template>
-  <h1 id="logoText">Lakeside Sports</h1>
-  <Navbar></Navbar>
-  <h1>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, odio tenetur
-    ea ducimus aliquam, animi sapiente, soluta provident amet culpa perspiciatis
-    distinctio. Unde fugiat aut atque, non laboriosam dolores autem eaque
-    voluptas, velit iusto eveniet neque ipsam nihil est officiis nam animi sit.
-    Fugiat voluptas ipsa harum earum, animi in dicta, ea vitae, molestias amet
-    reprehenderit eveniet et numquam fugit ipsum. Illum nesciunt facilis quidem
-    eum aspernatur minima assumenda adipisci repudiandae vitae cumque harum
-    consequatur quo officia laboriosam iusto sunt recusandae optio enim aliquid,
-    magni tempore non obcaecati vel eius? Earum perferendis esse tempore iste
-    aperiam, perspiciatis fugiat reprehenderit corrupti libero, cum animi sunt
-    delectus unde consectetur ratione enim repellat at optio magni minima est
-    dolorem sapiente doloremque laboriosam. Quo facere culpa praesentium libero
-    explicabo animi, incidunt similique harum! Nisi delectus autem eos ducimus.
-    Vitae, itaque impedit. Exercitationem placeat dolore nihil illo dignissimos
-    dicta quaerat aliquid, nobis sequi officia magnam vel voluptatem possimus
-    velit, alias maxime quia nam. Distinctio magni iste quo possimus ratione
-    fugit, soluta eum minus eius laborum, vero incidunt itaque. Error molestiae
-    at minima itaque, sint perferendis facere eligendi quaerat accusamus placeat
-    velit pariatur voluptatem aperiam dolorum quos? Eaque, deserunt. Laboriosam
-    accusantium debitis ratione excepturi voluptate harum suscipit sint optio
-    eligendi! Tempora, facere ipsa saepe illo velit labore dolorum aspernatur
-    delectus reiciendis beatae? Quibusdam laudantium fugiat libero, accusamus
-    nostrum placeat doloremque repudiandae modi, sapiente nam, illo deserunt
-    ipsam. Aliquam, consequatur! Pariatur perspiciatis, a doloribus veritatis
-    vitae quibusdam porro quis corporis fuga atque nostrum dignissimos quisquam
-    iusto architecto possimus maiores dolor similique optio, minima sapiente
-    excepturi blanditiis, consectetur voluptatibus itaque. Aspernatur delectus
-    eius inventore consequatur rem maiores quibusdam voluptas, ad optio alias,
-    earum saepe excepturi nostrum cum tempora dolorem modi! Qui accusamus
-    voluptatibus temporibus nobis quibusdam sequi corporis tenetur quis
-    cupiditate maxime reprehenderit et, eius nemo beatae magnam molestiae
-    ratione rem suscipit expedita illum debitis eum saepe enim quidem? Id
-    tempora pariatur inventore atque incidunt excepturi ullam, dolores
-    laboriosam molestias itaque tempore aut voluptatem voluptate facere
-    veritatis est aliquid saepe! Placeat laboriosam incidunt adipisci quam
-    perferendis, est deserunt veniam modi et minima quod cupiditate! Vero quo
-    consequuntur quas corrupti voluptate blanditiis sint iure? Sunt totam quae
-    asperiores non quod? Quo nam consequuntur totam rem quidem, architecto
-    deleniti, eum quaerat reiciendis quis, asperiores obcaecati fugit animi.
-    Nesciunt provident corporis suscipit vero incidunt nisi, saepe quisquam, non
-    at eos rerum molestiae, unde architecto nihil placeat minima itaque fugiat
-    ipsa quidem hic ducimus? Possimus, consequuntur dolore optio, aliquid odio
-    doloribus recusandae necessitatibus tenetur, quibusdam ut est! Harum magni a
-    reiciendis aspernatur nostrum omnis ducimus, sapiente earum doloribus ut
-    culpa maiores sit quae natus molestiae suscipit. Fuga eius eaque, repellat
-    recusandae illo eos veniam molestias aspernatur ex ea laboriosam quas ipsa
-    illum atque tenetur id praesentium neque fugiat aliquam velit, voluptatem
-    commodi sequi beatae cupiditate? Architecto illo assumenda quae! Voluptatum
-    dignissimos eaque quisquam voluptate numquam voluptas, laborum odit natus
-    explicabo temporibus. Quo aperiam possimus itaque quia numquam. Labore
-    soluta cupiditate praesentium cumque voluptatum, illo accusantium, maiores
-    amet iusto ullam nisi voluptates repellendus. Voluptatibus hic dolor illo
-    autem soluta aut nam ea est repellat aliquid rerum omnis, suscipit doloribus
-    quibusdam labore eaque non, in natus ipsum, voluptates eveniet quisquam
-    deleniti. Unde ducimus accusamus reiciendis laboriosam voluptatem nulla
-    sapiente distinctio? Voluptate velit exercitationem vel aut molestias facere
-    reiciendis impedit fuga dicta! Soluta unde voluptas, ducimus similique
-    dolorum eligendi nulla quia tempore alias pariatur iusto eaque aspernatur,
-    eius inventore dolor ipsam doloremque laborum at rem atque debitis autem
-    facere! Quaerat laboriosam consequatur impedit eaque, harum vel quisquam
-    libero nam culpa neque sit rem laudantium, error tenetur similique
-    asperiores numquam ipsum unde. Deserunt ut in dolore commodi eum tempora
-    maxime nesciunt voluptates cum veritatis voluptate, obcaecati velit eveniet.
-    Corrupti maxime veniam obcaecati quam veritatis consectetur eum nulla
-    tempore nostrum dolorem sapiente asperiores natus quasi impedit officiis
-    excepturi molestiae iusto, dolore numquam saepe debitis ex. Exercitationem
-    magni assumenda laboriosam illo, laborum id nulla saepe voluptates quas
-    eaque vel quo, magnam blanditiis hic tempore. Harum quidem nostrum odit
-    magni sit veniam molestias, iure modi possimus enim illum, quia aliquam
-    dolorum doloribus? Sint, pariatur dignissimos debitis excepturi vitae velit
-    placeat aut saepe consequuntur ex facere autem non, minima voluptates
-    architecto eius illo necessitatibus, odio soluta voluptatum ea tempore
-    veritatis. Odio quos porro voluptates saepe corporis ratione, tempore
-    reprehenderit sunt doloribus tenetur qui reiciendis dolorum magni ipsum
-    minus quis architecto nemo? Nihil mollitia ipsa facilis, ducimus odio sunt
-    doloribus fugiat quo sequi nam placeat a tempora saepe animi, harum soluta?
-    Natus autem neque unde necessitatibus libero rerum ratione vero aliquam
-    accusantium, minus fugiat, voluptatibus aperiam exercitationem nostrum
-    corporis quae repellat numquam, vel impedit ullam cum! Atque, veritatis sed
-    unde in odio nostrum debitis. A numquam consectetur ut recusandae,
-    dignissimos dolores, quaerat enim, fuga consequuntur tenetur itaque ex
-    placeat! Suscipit consequatur iste itaque, repellendus nesciunt numquam
-    sapiente dolore rem cumque qui magnam, recusandae cupiditate, vitae aliquam
-    at velit. Cumque ipsum sequi ea iste, sed voluptatum nostrum cum dolores
-    incidunt sint soluta sunt amet explicabo molestiae, quo aspernatur. Neque
-    cumque magnam ratione, aliquid minus odio quasi veniam amet, exercitationem
-    doloribus sed. Ullam, possimus aspernatur repellendus architecto ab velit
-    harum quisquam. Eligendi laboriosam reprehenderit ad a fuga quia architecto
-    pariatur delectus soluta aliquid. Recusandae distinctio veniam nam minima
-    quod quo eaque et, laborum veritatis incidunt, tenetur nisi pariatur maxime
-    aliquid dolores excepturi consequuntur cum, quibusdam sit dolorum! Debitis
-    nemo beatae ipsum consequatur, vel, numquam culpa tenetur excepturi
-    consectetur aliquid quaerat voluptates deserunt est!
+  <div class="bar">
+    <nav>
+      <ul class="nav-bar">
+        <li class="item"><a class="task-item" href="#">Home</a></li>
+        <li class="item"><a class="task-item" href="#">Activities</a></li>
+        <li class="item"><a class="task-item" href="#">Blog</a></li>
+        <li class="item"><a class="task-item" href="#">Pricing</a></li>
+        <li class="item"><a class="task-item" href="#">Contact Us</a></li>
+      </ul>
+    </nav>
+  </div>
+  <h1 class="Heading">
+    Lakeside
+    <h1
+      class="Heading"
+      style="font-family: 'Amatic SC', cursive; font-weight: 700"
+    >
+      Watersports
+    </h1>
   </h1>
+
+  <div class="container">
+    <p style="margin: 7vh">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex sapiente,
+      deserunt necessitatibus exercitationem aliquam corporis, aut quia impedit
+      distinctio cupiditate aliquid commodi, laboriosam voluptate. Distinctio
+      animi esse a, dignissimos, ullam ipsa hic reprehenderit sint dolores saepe
+      explicabo expedita debitis necessitatibus qui corporis voluptatem.
+      Laboriosam eaque culpa dolorem, omnis dolores quod quia recusandae
+      officiis voluptates quis. Velit qui repellendus dignissimos vero quia ea
+      sed placeat, libero nostrum aliquid eaque odio temporibus perferendis
+      eligendi beatae! Ratione, facilis facere. Doloremque corrupti dolorum
+      saepe laborum nemo illum, adipisci numquam expedita tempora ea voluptas
+      enim eos earum officiis quam qui optio alias? Soluta sunt aperiam
+      assumenda facere nobis nesciunt quisquam incidunt quaerat cupiditate
+      libero porro placeat, deleniti rem provident voluptate a. Quo non deserunt
+      error consequuntur temporibus quasi, animi autem. Beatae ducimus in
+      quisquam! Similique, nobis ut eos magni voluptate perspiciatis modi
+      quibusdam sit, alias fugiat dolorem, dolor officia earum at. Et
+      perspiciatis impedit, illo, aspernatur quibusdam quis officiis voluptates
+      earum quisquam, esse a. Corporis facilis ipsam quae pariatur? Dolore
+      similique quod possimus perspiciatis amet qui quis minus molestiae
+      explicabo eaque, placeat ullam fuga suscipit nulla eos iste nesciunt,
+      itaque reiciendis doloribus, architecto libero excepturi dolor aperiam
+      vel. Quibusdam cum iure reprehenderit repudiandae consequuntur minima
+      officiis, esse deleniti ratione molestias explicabo ipsum itaque nisi
+      suscipit rem non earum, aliquam nobis illum, delectus magnam? Voluptates,
+      quaerat perspiciatis blanditiis provident aperiam amet reiciendis quisquam
+      deleniti, praesentium dolorem quod corrupti, dolores repudiandae.
+      Voluptatibus earum at possimus labore quos et corporis nam, animi non
+      ipsum molestiae odio dolorum reiciendis, sequi velit ullam, dolor suscipit
+      maxime mollitia fuga soluta laboriosam unde. Libero reprehenderit quaerat
+      perferendis saepe, sequi at expedita eligendi porro doloribus, magni
+      provident aut amet eveniet atque ullam. Error ipsum, blanditiis voluptas
+      excepturi accusantium sed quia mollitia tenetur exercitationem dolorem
+      fugit sequi similique, nobis, veritatis eaque. Molestiae praesentium, sed
+      tempore non et asperiores architecto exercitationem ipsa, excepturi fugiat
+      voluptates ea veritatis provident accusantium laborum ab incidunt amet
+      minus iusto sapiente. Reiciendis aspernatur tenetur quia possimus
+      excepturi, odit quam quae qui ea tempora at! Dicta nesciunt, corporis
+      labore suscipit doloremque odio reprehenderit, dolorem, sit porro aliquam
+      in ut perspiciatis saepe commodi? Labore, aspernatur tenetur. Enim optio
+      sunt aut voluptatem quia officia neque in quidem laudantium accusamus
+      cumque tenetur, maxime repudiandae pariatur possimus corporis quod atque
+      nisi doloribus odit molestiae asperiores error iusto eligendi. Tempore
+      natus repellendus ullam totam exercitationem nihil fuga aliquid? Iure eos
+      odio consequatur necessitatibus in laboriosam recusandae, dolore
+      voluptatem? Beatae magnam nihil sunt placeat repudiandae maxime numquam
+      veritatis repellendus recusandae, delectus dolores, voluptates assumenda.
+      Iure nisi quaerat nam numquam unde tempore debitis, laborum autem velit
+      doloribus voluptatem iusto! Cum dignissimos dolore ipsa ducimus alias
+      natus nesciunt placeat! Earum nam nulla itaque! Tempora perferendis
+      reiciendis fugit delectus mollitia veritatis asperiores, assumenda
+      distinctio a. Facilis vel eum voluptas? Totam id nam ducimus repellendus
+      vitae quidem, eum molestiae ullam ratione. Dolores impedit quisquam
+      aliquam, quos iure ea in. Minima, magni distinctio! Neque nobis
+      exercitationem, alias nam, vitae recusandae consequuntur placeat veniam
+      amet earum veritatis. Provident amet esse, eaque maxime nemo eius
+      laboriosam saepe pariatur magnam tenetur id blanditiis. Est consectetur
+      recusandae sit, enim eum voluptatum consequatur? Sequi ratione voluptates
+      eveniet perspiciatis. Labore provident deleniti quidem dolores et, aut
+      aliquam incidunt earum dolorum assumenda modi possimus unde commodi
+      mollitia sed voluptas repudiandae! Pariatur ea quod architecto quisquam at
+      aspernatur numquam iusto et excepturi omnis asperiores, dolorum facere
+      laboriosam eaque quas cum amet magnam. Saepe esse cum illo error
+      voluptatem laudantium distinctio debitis nulla dolorem architecto
+      praesentium asperiores sequi dignissimos expedita, soluta fugit qui? Vero
+      rem laudantium reiciendis debitis suscipit quos deleniti voluptate tenetur
+      voluptatem culpa quae iure nulla dignissimos enim illo, deserunt corrupti
+      voluptates nisi corporis sed dolorem praesentium nemo. Quaerat deserunt
+      voluptatum totam distinctio rerum cumque facilis impedit ab nesciunt nobis
+      suscipit sed, similique error iure eius, doloribus ipsa. Non repellat
+      nihil magni porro nobis facilis vero cumque ex a expedita sapiente
+      assumenda labore fuga blanditiis id repellendus necessitatibus neque
+      deleniti accusantium exercitationem consequatur, nostrum voluptatem
+      itaque. Eligendi quibusdam velit quidem inventore nesciunt ipsam.
+      Consequuntur voluptas enim dolor labore! Voluptates sint blanditiis eius
+      animi? Explicabo doloribus numquam vero odit minima a quos laboriosam
+      officia? Ratione blanditiis, nihil tempore animi minima corporis tenetur
+      non deleniti adipisci sapiente earum nisi nulla reiciendis mollitia labore
+      odit quis quod, vero quas, iste porro inventore! Earum, ratione cum id
+      dolorum, atque necessitatibus nemo, incidunt consectetur saepe recusandae
+      modi. Necessitatibus ipsam cumque quidem minus? Est sequi facilis,
+      deserunt officiis maiores voluptatum placeat officia, eius distinctio
+      dicta, quas nam laboriosam consectetur sapiente atque tempore iusto dolore
+      quisquam! Dolorum distinctio unde praesentium, architecto, nesciunt
+      debitis necessitatibus hic reprehenderit id magni amet laboriosam porro
+      sint minus ipsa repellat veniam similique facere temporibus in quas
+      dolorem omnis voluptatibus modi. Asperiores, cumque rem perferendis
+      dolorem molestiae ducimus libero cum alias voluptas ut sapiente! Ratione
+      beatae voluptates aliquam, est nesciunt deserunt cumque!
+    </p>
+    <div style="background-color: rebeccapurple">Shoop</div>
+  </div>
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Tapestry&display=swap");
+:root() {
+  --primary-font: "Ubuntu", sans-serif;
+  --primary-weight: 400;
+}
 
 html,
 body,
-main {
+main,
+h1,
+h2,
+h3,
+div {
   margin: 0;
   padding: 0;
 }
@@ -113,11 +139,80 @@ main {
   font-family: "Amatic SC", cursive;
   font-size: 9vh;
   background-image: url("logo_bg.jpg");
-  padding: 5%;
+  padding: 2%;
   background-repeat: no-repeat;
   background-size: cover;
-  background-position: bottom center;
+  background-position: bottom left;
   margin: 0;
   z-index: -1;
+}
+
+.container {
+  margin-inline: 20vw;
+  align-items: center;
+  align-content: center;
+  align-self: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: rgba(240, 248, 255, 0.877);
+}
+
+.Heading {
+  align-self: center;
+  text-align: center;
+  font-family: "Amatic SC", cursive;
+  color: rgb(0, 0, 0);
+  font-weight: 400;
+  font-size: 5em;
+  margin: 5vh;
+  line-height: 3vh;
+}
+
+html {
+  background-image: url(bg.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.bar {
+  padding-top: 1vh;
+  padding-bottom: 1vh;
+}
+
+.nav-bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.item {
+  list-style: none;
+  margin-inline: 3vw;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+.task-item {
+  color: black;
+}
+
+.task-item:link {
+  color: black;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+.task-item:hover {
+  color: black;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+.task-item:active {
+  color: black;
+  background-color: transparent;
+  text-decoration: none;
 }
 </style>
